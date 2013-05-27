802a96977749626298eeff716a5548e7c92998bd
Dustin Dettmer <dustin@poshero.com>
2013-05-26 18:45:58 -0700

    Turning on django.

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'd7m7jce5jjqg3j',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'kgswfsdqehalcv',
+        'PASSWORD': 'O7wDXGl415t2OL0CqeATMay9od',
+        'HOST': 'ec2-23-21-130-189.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
